6bf03fe695ebac9bf112c1f0f5dc0289d9ed95f0
Oleksii Tumanov <oleksii.tumanov@gmail.com>
2015-04-29 19:22:02 +0300

    webService. Save and get Avatar

--- src/main/java/com/sfeir/endpoint/YourFirstAPI.java
+++ src/main/java/com/sfeir/endpoint/YourFirstAPI.java
@@ -1,8 +1,8 @@
 package com.sfeir.endpoint;
 
 /**
-  * Add your first API methods in this class, or you may create another class. In that case, please
-  * update your web.xml accordingly.
+ * Add your first API methods in this class, or you may create another class. In that case, please
+ * update your web.xml accordingly.
  **/
 
 import com.google.api.server.spi.config.Api;
@@ -11,14 +11,18 @@ import com.google.api.server.spi.config.ApiNamespace;
 import com.google.gdata.util.ServiceException;
 
 import javax.inject.Named;
+import java.io.File;
 import java.io.IOException;
 
-/** An endpoint class we are exposing */
+
+/**
+ * An endpoint class we are exposing
+ */
 @Api(name = "myApi",
         version = "v1",
         namespace = @ApiNamespace(ownerDomain = "endpoint.sfeir.com",
                 ownerName = "endpoint.sfeir.com",
-                packagePath=""))
+                packagePath = ""))
 
 public class YourFirstAPI {
 
@@ -27,13 +31,32 @@ public class YourFirstAPI {
         AuthResult response = new AuthResult();
 
         try {
-            response.setAuthorized(ReadSpreadsheet.authenticate(login,pwd));
+            response.setAuthorized(ReadSpreadsheet.authenticate(login, pwd));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ServiceException e) {
             e.printStackTrace();
         }
         return response;
+    }
+
+    @ApiMethod(name = "saveAvatar")
+    public void saveAvatar(@Named("login") String login, @Named("avatar") File avatar) {
+        try {
+            AvatarStorage.writeToFile(login, avatar);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ApiMethod(name = "getAvatar")
+    public File getAvatar(@Named("login") String login) {
+        try {
+            return AvatarStorage.readFromFile(login);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
